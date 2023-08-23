727fc39639939744179808e3988f183137d6853a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-23 20:19:56 -0300

    desafio2

---
+++ desafio/desafio2.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Temperatura conversão")
+    var celsius: Float = 60.0f
+    val kelvin : Float  = celsius + 273
+    val fahrenheit : Float  = celsius * 1,8 + 32 
+    println("Celsius: "+ celsius)
+    println("Fahrenheit: "+ fahrenheit)
+    println("Kelvin: " + kelvin)
+
+
+}
+
+
