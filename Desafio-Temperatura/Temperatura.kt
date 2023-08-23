5635febbcb9b75fc2569ff079f8c19a070d34668
Thiago Castro <rm97136@fiap.com.br>
2023-08-23 20:00:01 -0300

    Realizando o desafio conversor de temperatura

---
+++ Desafio-Temperatura/Temperatura.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val celsius: Double = 25.0
+    val fahrenheit = celsius * 9/5 + 32
+    val kelvin = celsius + 273.15
+
+    println("Temperatura em Celsius: $celsius °C")
+    println("Temperatura em Fahrenheit: $fahrenheit °F")
+    println("Temperatura em Kelvin: $kelvin K")
+
+}
