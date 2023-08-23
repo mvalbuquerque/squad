3764645bbe96d37cc25b201c450e1965ac7ae034
Matheus Ramos de Pierro <matpierro570@gmail.com>
2023-08-23 20:10:18 -0300

    Conversor de temperatura em kotlin

---
+++ src/conversorTemperatura.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val celsius = 25.0
+
+    println("A temperatura em graus Celsius é:" + celsius)
+    println("A temperatura em Fahrenheit é:" + celsius * 9/5 + 32)
+    println("A temperatura em Kelvin é:" + celsius + 273.15)
+
+}
