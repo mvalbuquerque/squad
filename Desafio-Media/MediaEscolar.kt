cf69d6ecff91ac48e7dd6f9a8957189aa740a003
Thiago Castro <rm97136@fiap.com.br>
2023-08-23 19:27:01 -0300

    Desafios resolvidos

---
+++ Desafio-Media/MediaEscolar.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var nota1:Double = 8.5
+    var nota2:Double = 7.0
+    var nota3 = 10.0f
+    var result = (nota1 + nota2 + nota3) / 3
+    println("Resultado da sua média é: $result")
+}
---
+++ Desafio-Perfil/PerfilUsuario.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var nome:String = "Thiago"
+    var email:String = "thi.castro@mail.com"
+    val idade:Int = 19
+    var atividade:Boolean = true
+    println("Meu nome é $nome, tenho $idade anos, email para contato: $email")
+    println("Usuario ativo: $atividade")
+}
